41497027b00d50f576edaea356afe62eb1498088
progame1201 <progamehelpus2@gmail.com>
2025-06-29 19:13:59 +0300

    upd

--- objects.py
+++ objects.py
@@ -10,11 +10,3 @@ class Cell:
         self.mine = False
         self.opened = False
         self.flagged = False
-
-class Flag(pygame.sprite.Sprite):
-    def __init__(self, x, y, image):
-        super().__init__()
-        self.image:pygame.image = image
-        self.rect = image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
